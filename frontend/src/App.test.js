6e13a087de0dac8fb888f4f37d3a36d1cd8d5fe8
Lindonor Neto <netoespl@hotmail.com>
2024-12-07 15:33:39 -0300

    test: atualiza testes do `App`

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import * as useProductsHook from './hooks/useProducts';
+import mockProducts from './mocks/mockProducts';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('Renderiza load', () => {
+    jest.spyOn(useProductsHook, 'default').mockReturnValue({
+      products: mockProducts,
+      features: [mockProducts[0].features],
+      preferences: [mockProducts[0].preferences],
+      isLoading: true,
+    });
+
+    render(<App />);
+
+    const logo = screen.getByRole('img');
+
+    expect(logo).toHaveAttribute(
+      'src',
+      'https://d3jj9yc7rgpax4.cloudfront.net/brand-system/logos/rdstation-logo-filled-default-horizontal.svg'
+    );
+  });
 });
