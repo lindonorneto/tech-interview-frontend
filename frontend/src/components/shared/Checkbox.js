65646d03a73fb7c74af80be56483718890e1e72d
Lindonor Neto <netoespl@hotmail.com>
2024-12-07 10:57:12 -0300

    feat: adiciona `cursor-pointer` nos checkbox

--- frontend/src/components/shared/Checkbox.js
+++ frontend/src/components/shared/Checkbox.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Checkbox({ children, ...props }) {
   return (
-    <label className="flex items-center">
-      <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-500" {...props} />
+    <label className="flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        className="form-checkbox h-5 w-5 text-blue-500"
+        {...props}
+      />
       <span className="ml-2">{children}</span>
     </label>
   );
